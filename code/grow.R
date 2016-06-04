e3a15724459ee41f08c701a103506f09dfadf9d2
Lucas Roberts <rlucas7@vt.edu>
2016-06-04 12:04:35 -0400

    added files for the grow function and the split_rule.R function which is called by the split_rule.R function

---
+++ code/grow.R
@@ -0,0 +1,26 @@
+#
+#
+#
+#
+#==============================================
+
+
+library(tree)
+example(tree)
+str(ir.tr)
+
+## function to implement CGMs grow function
+
+grow <- function(arg1=ir.tr, data_nm=iris){
+	
+	split_rule <- function(arg1=ir.tr, data_nm=ur_data)
+	
+	## NOW SPLIT VALUE IS THE VALUE TO RETURN...
+	# split_val # UNCOMMENT WHEN CUT-N-PASTE INTO SEPARATE FUNCTION
+	
+	# now we need to update the tree with the new split rule: 
+	
+	### CAN we chunk this piece out into its' own function as well? 
+	### NOTE1: requires tree object, and split rule
+	
+}
---
+++ code/split_rule.R
@@ -0,0 +1,51 @@
+### CAN WE CHUNK THIS BIT OF CODE INTO A DEPENDENT FUNCTION??
+	### NOTE1: THIS IS WORKING FOR NUMERIC, ALSO NEED FACTOR...
+	### NOTE2: THE WHILE LOOP IS INFINITE IF <3 OBS ... (REALLY?)
+	
+split_rule <- function(arg1=ir.tr, data_nm=ur_data){
+	#first get all the terminal nodes 
+	t_nodes <- get_term_nodes(arg1)
+	# next pick one at random 
+	selected_node <- sample(t_nodes, 1)
+	# now we want to get all the observations within selected node
+	tnode_obs <- get_term_node_nums(arg1)
+	#subset the data to the factors in the tree(if needed)
+	factors <- dimnames(attr(arg1$terms, 'factors'))[[2]]
+	factor_data <- data_nm[factors]
+	# logic vector of which rows to sample from
+	subset_data <- factor_data[tnode_obs == selected_node,]
+	#select the splitting factor
+	column <- sample(1:length(factors),1)
+	split_factor_vals <- subset_data[,column]
+	### code differs if the are categorical vs numeric
+	bflag <- TRUE
+	if(is.numeric(split_factor_vals)){		
+		while(bflag ==TRUE){
+		split_val <- split_factor_vals[sample(1:length(split_factor_vals),1)]
+		# now check if split val has obs in the two new leaf nodes
+		stop_criteria <- sum(1*(split_factor_vals < split_val)) < length(split_factor_vals)
+		# if stop criteria == TRUE stop else, repeat 
+		if(stop_criteria == TRUE){bflag <- FALSE}
+		}else{ # factor is categorical
+			while(bflag ==TRUE){
+		len <- length(unique(as.numeric(iris$Species)))
+		sample(1:(len-1),1)
+		# now check if split val has obs in the two new leaf nodes
+		stop_criteria <- sum(1*(split_factor_vals < split_val)) < length(split_factor_vals)
+		# if stop criteria == TRUE stop else, repeat 
+		if(stop_criteria == TRUE){bflag <- FALSE}	
+			
+			}
+	}
+	ret_obj <- c(split_val, factors[column], selected_node)
+	names(ret_obj) <- c('split_value','factor_name', 'node_number')
+	ret_obj
+}
+
+
+#simple beta test(s)
+
+split_rule(arg1=ir.tr, data_nm=iris)
+# returns something like: 
+#split_value   factor_name   node_number 
+#          "3" "Sepal.Width"          "14" 
