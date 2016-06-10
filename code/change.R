60157b44c7907673eb2acd607b95672ad375c262
Lucas Roberts <rlucas7@vt.edu>
2016-06-09 20:52:40 -0400

    added a file and started to code out the change function in CGMs proposal distribution

---
+++ code/change.R
@@ -0,0 +1,15 @@
+#
+#
+#
+#
+# code to implemt the change function for the proposal in CGM x
+#
+#=================================================================
+
+change <- function(arg1=ir.ir){
+	
+	inter_node_list <- get_int_nodes(arg1)
+	inter_node_list 
+	
+	
+}
