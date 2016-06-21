816909244f29588fded32f451251d8ba3d089657
Lucas Roberts <rlucas7@vt.edu>
2016-06-20 20:15:30 -0400

    added some more updates to the simulation, this code needs more work

--- code/simulation1.R
+++ code/simulation1.R
@@ -57,8 +57,64 @@ lines(0:25,dpois(0:25, lambda=mean(y))  )
 
 ### second setup data structures needed for the MCMC sampler 
 
+data_frame <- data.frame(y,x1,x2)
+
+# m: the number of MCMC draws within a chain
+m <- 1000  
+# p: the number of restarts to the MCMC chain
+p <- 1 
+
+# the binary vector (1=MCMC stays at current state on next iteration, 0=go to proposed state)
+stay_or_go <- matrix(1,nrow=m*p, ncol=1)
+
+# log-likelihood measure
+llhood <- matrix(0.1, nrow=m*p, ncol=1)
+
+# list of sampled trees 
+samp_tree_list <- vector(mode='list', length=m*p)
+
+# initial decision tree fit 
+
+init_tree <- tree(y~x1+x2, data=data_frame)
+plot(init_tree,type='unif')
+text(init_tree)
 
 ### sample the decision trees 
+
+for(i in 1:m){
+	
+	propose_choice <- sample(1:2, 1)
+	if(propose_choice == 1){ 
+		### grow step proposal 
+		proposed_tree <- grow(init_tree, data_nm=data_frame)
+		
+		}else{
+			### this is the prune step proposal
+		proposed_tree <- prune(init_tree)	
+			
+		}	
+	### now calculate the log-likelihood of the tree. 
+		prop_llhood <- calc_llhood()
+
+	### need a function that takes in grow/prune and gives the llhood `correct' terms
+	
+	### 
+	log_ratio <- llhood[i-1] - prop_llhood
+	
+	#### if/else criteria for stay or go in MCMC 
+	if(-rexp(1) > log_ratio ){
+		# go! 
+		llhood[i] <- prop_llhood
+		samp_tree_list[i] <- proposed_tree
+		stay_or_go[i] <- 0
+	}else{
+		#stay!
+		llhood[i] <- llhood[i]		
+		samp_tree_list[i] <- samp_tree_list[i-1]
+		stay_or_go[i] <- 1
+	}
+
+}# end of for(i in 1:m){} loop
 
 
 ### now calculate the convergence diagnostics 
