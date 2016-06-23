eb98ae2ff86d96add05dff9fe195f4ada6b48343
Lucas Roberts <rlucas7@vt.edu>
2016-06-22 22:24:07 -0400

    added a sum at the end of the function to get at the whole tree level. Still an issue somehwere b/c we don't have negative values once we take logs

--- code/llhood.R
+++ code/llhood.R
@@ -38,10 +38,10 @@ llhood_calc <- function(arg1=ir.tr, data_nm =ir.tr, a=1,b=1, al=1, bl=1){
 		n_0[i] <- sum( arg1$y[arg1$where == i_node] == 0 )
 		y_plus[i] <- sum( arg1$y[arg1$y[arg1$where == i_node] != 0 ])
 		n_plus <- tnode_nobs_table[i] - n_0[i]
-		any <- yplus[i] + al
+		any <- y_plus[i] + al
 		pytx[i] <- log_sigma(n_0[i]) + ( gamma(any) / (gamma(al)*bl^al) ) * ( n_plus + 1/bl)^(any)
 	}
-	log(pytx)
+	sum(log(pytx))
 }
 
 
